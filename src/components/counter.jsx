79122aaced8bd52f4b16739a87124b86244ed50a
jordy muniz <jordy.muniz96@gmail.com>
2021-07-28 13:19:47 -0500

    binding event handlers and conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,18 +4,48 @@ class Counter extends Component {
     //state is a special react property that holds any data that this component needs
     state = {
         count : 0,
+        // tags: ['tag1', 'tag2', 'tag3']
      };
 
-     styles = {
-        fontSize: 50,
-        fontWeight: 'bold'
+    //  constructor() {
+    //      super();
+    //      this.handleIncrement = this.handleIncrement.binf(this);
+    //  }
+
+    //  renderTags(){
+    //      if (this.state.tags.length === 0) return <p>There are no tags!</p>
+    //     return <ul>{ this.state.tags.map(tag => <li key ={tag}>{ tag }</li>) }</ul>
+    //  }
+
+    //  styles = {
+    //     fontSize: 50,
+    //     fontWeight: 'bold'
+    //  }
+
+     handleIncrement = (product) => {
+         console.log(product + 1)
+         this.setState({ count: this.state.count + 1 })
+         // obj.method(); this. return obj
+         // function(); this. returns refrence to the window object
+         //in strict mode this. returns undefined
+     }
+
+     doHandleIncrement = () => {
+         this.handleIncrement({id : 1})
      }
 
     render() { 
         return (
         <div>
+            {/* javascript can make logical && operator be equal to a non boolean type. the first half is the condition and the second half after the && is what you want to return
+            {this.state.tags.length === 0 && 'Please create a new tag!'}
+            {this.renderTags()} */}
             <span className = {this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className = "btn btn-secondary btn-sm">Increment</button>
+            <button 
+                onClick={this.handleIncrement}
+                className = "btn btn-secondary btn-sm">
+                    Increment
+            </button>
         </div>
         );
     }
@@ -30,6 +60,7 @@ class Counter extends Component {
         const { count } = this.state;
         return count === 0 ? "Zero" : count;
     }
+
 }
  
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 //imports bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
